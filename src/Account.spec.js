0f7ca157344f5d77a04c2a37aeb338adfa9d5a50
David Ewan Campbell <ewan.david@gmail.com>
2022-09-22 11:49:47 +0100

    5th commit - Added test for user story 2 to withdraw from account. Test expectation failing, removed single class instance from test top of test file & created instance for each test - passing

--- src/Account.spec.js
+++ src/Account.spec.js
@@ -1,14 +1,21 @@
 const Account = require("./Account");
 
 describe("Account", () => {
-  let account = new Account();
-
   it("starts user account with a balance of zero", () => {
+    let account = new Account();
     expect(account.balance).toEqual(0);
   });
 
-  it("can let a user make deposits into their account", () => {
+  it("lets a user make a deposit into their account", () => {
+    let account = new Account();
     account.deposit(1000);
     expect(account.balance).toEqual(1000);
-  })
+  });
+  
+  it("lets a user make a withdrawal from their account", () => {
+    let account = new Account();
+    account.deposit(1000);
+    account.withdrawal(500);
+    expect(account.balance).toEqual(500);
+  });
 });
