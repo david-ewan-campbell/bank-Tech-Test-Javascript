5e888b414f80951fa76bb95db44bb42d1d9e0a99
David Ewan Campbell <ewan.david@gmail.com>
2022-09-22 10:46:58 +0100

    4th commit - Added test for 1st user story -user able to make deposits (account class instantiation tested in previous commit) - tests passing 100% coverage

--- src/Account.spec.js
+++ src/Account.spec.js
@@ -3,7 +3,12 @@ const Account = require("./Account");
 describe("Account", () => {
   let account = new Account();
 
-  it("Starts account with a balance of zero", () => {
+  it("starts user account with a balance of zero", () => {
     expect(account.balance).toEqual(0);
   });
+
+  it("can let a user make deposits into their account", () => {
+    account.deposit(1000);
+    expect(account.balance).toEqual(1000);
+  })
 });
